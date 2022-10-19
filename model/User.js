9393407586d961e04594cf26f336db73e6e08088
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-19 18:21:48 -0500

    file structure and model/User.js

---
+++ model/User.js
@@ -0,0 +1,47 @@
+const { Schema, model, SchemaTypes } = require("mongoose");
+
+const { reactionSchema } = require("./Thought");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+      trim: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
