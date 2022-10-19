ab298b5dc64c89778709ecba6930c09256e108c3
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-19 18:33:57 -0500

    user-routes.js and initialize npm

---
+++ routes/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const {
+  findAllUsers,
+  addUser,
+  deleteUser,
+  updateUsers,
+  addFriends,
+  deleteFriends,
+} = require("../../controllers/userController");
+
+router.route("/").get(findAllUsers).post(addUser);
+router.route("/:userId").delete(deleteUser);
+
+router.route(`/api/users/:userId/friends/:friendId`).delete(deleteUser);
+
+module.exports = router;
